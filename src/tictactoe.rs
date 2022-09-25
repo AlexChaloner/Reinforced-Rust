f657305e8a338988a323813413ed628718038f9a
AlexChaloner <alex.chaloner@outlook.com>
2022-09-25 10:47:09 +0100

    Add pretty print

--- src/tictactoe.rs
+++ src/tictactoe.rs
@@ -1,4 +1,4 @@
-use std::io;
+use core::fmt;
 
 #[derive(Clone)]
 #[derive(Debug)]
@@ -6,6 +6,16 @@ enum BoardEntry {
   Blank,
   X,
   O
+}
+
+impl fmt::Display for BoardEntry {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match self {
+      BoardEntry::Blank => write!(f, " "),
+      BoardEntry::X => write!(f, "X"),
+      BoardEntry::O => write!(f, "O"),
+    }
+}
 }
 
 /*
@@ -22,10 +32,17 @@ impl Board {
   fn put(&mut self, x: usize, y: usize, entry: BoardEntry) {
     self.board[x][y] = entry;
   }
-  // fn pretty_print(&self) {
-  //   let mut string = "";
-  //   for 
-  // }
+  fn pretty_print(&self) {
+    let mut string = String::from("+-+-+-+\n");
+    for y in 0..3 {
+      string.push('|');
+      for x in 0..3 {
+        string.push_str(format!("{}|", self.get(x, y)).as_str());
+      }
+      string.push_str("\n+-+-+-+\n");
+    }
+    println!("{}", string);
+  }
 }
 
 fn initialise_board() -> Board {
@@ -40,5 +57,5 @@ fn initialise_board() -> Board {
 pub fn tictactoe_board() {
   let mut board = initialise_board();
   board.put(0, 0, BoardEntry::X);
-  
+  board.pretty_print();
 }
