cb477f32c8624d256a9acc897d8d0bc231838fbb
AlexChaloner <alex.chaloner@outlook.com>
2022-10-08 20:30:43 +0100

    Implement proper traits

--- src/reinforcement_learning/generic_reinforcement_learner.rs
+++ src/reinforcement_learning/generic_reinforcement_learner.rs
@@ -1,23 +1,39 @@
 // Reinforcement learning module
 
-use rand::Rng;
-
 pub trait Action {
-
+    fn to_string() -> String;
 }
 
-pub trait State {
-
+pub trait State<A>
+where
+    A: Action
+{
+    fn to_string() -> String;
+    fn initial_state() -> Self;
+    fn next_state(action: &A) -> Self;
+    fn is_terminal() -> bool;
+    fn available_actions() -> Vec<A>;
+    fn num_available_actions() -> usize {
+        return Self::available_actions().len();
+    }
 }
 
-pub trait Policy {
-  fn get_action(state: &State) {}
+pub trait Policy<S, A>
+where 
+    S: State<A>,
+    A: Action
+{
+    fn get_action(state: &S) -> A;
 }
 
-pub trait ReinforcementLearner {
-  fn get_action_value(state: &State, action: &Action) {}
-  fn update_action_value(state: &State, action: &Action) {}
-  fn get_state_value(state: &State) {}
-  fn update_state_value() {}
+pub trait ReinforcementLearner<S, A> 
+where 
+    S: State<A>,
+    A: Action
+{
+    fn get_action_value(state: &S, action: &A);
+    fn update_action_value(state: &S, action: &A);
+    fn get_state_value(state: &S);
+    fn update_state_value(state: &S);
 }
 
